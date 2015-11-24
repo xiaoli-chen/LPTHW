07ff2844e24650a2b66ef455429bfd98186131a8
Xiaoli Chen <haier.cxl@gmail.com>
2015-11-24 14:48:46 -0800

    initial

---
+++ projects/skeleton/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Xiaoli Chen',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'xiaoli_chen@umail.ucsb.edu.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
